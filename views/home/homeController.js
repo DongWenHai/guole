dd778752a2284d728e9bb4b64042b3a31bea4157
DongWenHai <903767799@qq.com>
2018-01-09 21:17:25 +0800

    add init home

--- views/home/homeController.js
+++ views/home/homeController.js
@@ -1,7 +1,39 @@
 angular.module('app')
-	.controller('homeController',['$scope', '$state','$http',function($scope,$state,$http){
+	.controller('homeController',['$rootScope','$scope', '$state','$http','API','BombBox',function($rootScope,$scope,$state,$http,API,BombBox){
+		//init home data
+		$scope.data = {};
 
-		//头部按钮
+		function initdata(){
+			BombBox.loadingShow();
+			API.fetchGet('http://127.0.0.1:9000/home')
+				.then(function(data){
+					BombBox.loadingHide();
+					$scope.data = data.data;
+					selectproduct();
+				})
+				.catch(function(err){
+					BombBox.loadingHide();
+					console.log(err);
+				})
+		}
+		initdata();
+
+		function selectproduct(){
+			$scope.hotproducts = [];
+			$scope.necessaryProducts = [];
+			$scope.fashionProduct = [];
+			for(var i=0;i<$scope.data.products.length;i++){
+				if($scope.data.products[i].cSign == '1000'){
+					$scope.hotproducts.push($scope.data.products[i]);
+				}else if($scope.data.products[i].cSign == '1001'){
+					$scope.necessaryProducts.push($scope.data.products[i]);
+				}else if($scope.data.products[i].cSign == '1002'){
+					$scope.fashionProduct.push($scope.data.products[i]);
+				}
+			}
+		};
+
+		//header button control
 		$scope.muneSelected = false;
 		$scope.muneselect = function(){
 			$scope.muneSelected = $scope.muneSelected == false ? true : false;
@@ -12,94 +44,5 @@ angular.module('app')
 			autoPlay: "true",
 			slideInterval: "2000"
 		};
-
-		$scope.bannerimgsrc = [
-			'./images/banner1.png',
-			'./images/banner2.png',
-			'./images/banner3.png'
-		];
-		//产品类型
-		$scope.producttype = [
-			{
-				imgsrc:'./images/phone_i@3x.png',
-				name: '手机通讯类',
-				stateName:'phoneclassify'
-			},
-			{
-				imgsrc:'./images/car_i@3x.png',
-				name: '汽车点火类',
-				stateName:'carfireclassify'
-			},
-			{
-				imgsrc:'./images/computer_i@3x.png',
-				name: '笔记本数码类',
-				stateName:'notebookclassify'
-			},
-		];
-		//热门产品
-		$scope.hotproducts = [
-			{
-				imgsrc:'./images/gp04_i@3x.png',
-				name:'专利私模应急电源',
-				price:'310.00'
-			},
-			{
-				imgsrc:'./images/mili_i@3x.png',
-				name:'迷你果乐移动充',
-				price:'79.00'
-			},
-			{
-				imgsrc:'./images/ta_i@3x.png',
-				name:'果乐10000毫安大容量',
-				price:'179.00'
-			},
-			{
-				imgsrc:'./images/qc03_i@3x.png',
-				name:'新款高通QC3.0快充',
-				price:'125.00'
-			}
-		];
-		//户外必备
-		$scope.necessaryProducts = [
-			{
-				imgsrc:'./images/officei4_i@3x.png',
-				name:'超薄便携M13',
-				price:'130.00'
-			},
-			{
-				imgsrc:'./images/brand_i@3x.png',
-				name:'化妆盒移动电源',
-				price:'200.00'
-			},
-			{
-				imgsrc:'./images/empty_i@3x.png',
-				name:'弹夹式移动电源',
-				price:'130.00'
-			}
-		];
-		//时尚新品
-		$scope.fashionProduct = [
-			{
-				imgsrc:'./images/M13@3x.png',
-				name:'超薄便携M13',
-				price:'125.00'
-			},
-			{
-				imgsrc:'./images/zuixin_i@3x.png',
-				name:'化妆盒移动电源',
-				price:'75.00'
-			},
-			{
-				imgsrc:'./images/gun_i@3x.png',
-				name:'弹夹式移动电源',
-				price:'200.00'
-			}
-		];
-
-		$scope.goState = function(stateName){
-			$state.go(stateName);
-		}
-
-
 
 	}])
